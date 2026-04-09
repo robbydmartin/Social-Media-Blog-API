c95371e8bc89e604dede9b0cf38c494ad1bd38cc
Robby Martin <142545854+robbydmartin@users.noreply.github.com>
2026-04-09 05:37:49 -0400

    Update SocialMediaController.java

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -12,11 +12,6 @@ import Model.Message;
 import Service.AccountService;
 import Service.MessageService;
 
-/**
- * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
- * found in readme.md as well as the test cases. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
- */
 public class SocialMediaController {
 
     private MessageService messageService;
@@ -26,11 +21,7 @@ public class SocialMediaController {
         this.messageService = new MessageService();
         this.accountService = new AccountService();
     }
-    /**
-     * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
-     * suite must receive a Javalin object from this method.
-     * @return a Javalin app object which defines the behavior of the Javalin controller.
-     */
+
     public Javalin startAPI() {
         Javalin app = Javalin.create();
         app.get("example-endpoint", this::exampleHandler);
@@ -45,10 +36,6 @@ public class SocialMediaController {
         return app;
     }
 
-    /**
-     * This is an example handler for an example endpoint.
-     * @param context The Javalin Context object manages information about both the HTTP request and response.
-     */
     private void exampleHandler(Context context) {
         context.json("sample text");
     }
